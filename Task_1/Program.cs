d9cf67a543b3f83f43052d812b7170261c62d1df
caregor <caregor@gmail.com>
2022-07-21 13:06:40 +0300

    first commit

---
+++ Task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N 
+с помощью рекурсии. 
+M = 1; N = 5 -> "2, 4"
+M = 4; N = 8 -> "4, 6, 8" */
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+void recursion(int n, int m)
+{
+    if (n > m)
+    {
+        return;
+    }
+
+    if (n % 2 == 0)
+    {
+        System.Console.Write($" {n}");
+    }
+    
+    recursion(n + 1, m);
+}
+
+int n = Prompt("Enter N > ");
+int m = Prompt("Enter M > ");
+recursion(n, m);
+System.Console.WriteLine();
